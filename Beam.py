e421009a1602cf686589776d6d2ff6d5815504d9
Jim <jim.vandoren@gmail.com>
2022-03-20 21:58:41 +0100

    Cleanup

--- Beam.py
+++ Beam.py
@@ -3,14 +3,18 @@ import Part
 from Truss import BeamGui
 
 class Beam():
-    def __init__(self, object):
+    def __init__(self, object, line, width, height):
 
         object.Proxy = self
         object.addProperty('App::PropertyString', 'Description', 'Base', 'Beam description').Description = "This is a beam"
         object.addProperty('App::PropertyString', 'Type', 'Base', 'Object type').Type = "Beam"
-        object.addProperty('App::PropertyLength', 'Length', 'Dimensions', 'Box length').Length = '200 cm'
-        object.addProperty('App::PropertyLength', 'Width', 'Dimensions', 'Box width').Width = '10 cm'
-        object.addProperty('App::PropertyLength', 'Height', 'Dimensions', 'Box height').Height = '10 cm'
+        object.addProperty('App::PropertyLength', 'Length', 'Dimensions', 'Box length').Length
+        object.addProperty('App::PropertyLength', 'Width', 'Dimensions', 'Box width').Width
+        object.addProperty('App::PropertyLength', 'Height', 'Dimensions', 'Box height').Height
+
+        object.Length = line.Length
+        object.Width = width
+        object.Height = height
 
     def execute(self, object):
         """
@@ -20,17 +24,22 @@ class Beam():
         mybeam = Part.makeBox(object.Length, object.Width, object.Height)
         object.Shape = mybeam
 
-def test(objectName="myBeam"):
-    """ Add a beam to a document, used for testing """
+def test():
+    '''
+    Create a simple beam, for testing
+    '''
 
     document = FreeCAD.ActiveDocument
     if not document:
         document = FreeCAD.newDocument()
-
-    beamObject = document.addObject("Part::FeaturePython", objectName)
-    Beam(beamObject)
+    
+    v0 = FreeCAD.Vector(-500, 50, 50)
+    v1 = FreeCAD.Vector( 500, 50, 50)
+    line = Part.makeLine(v0, v1)
+    
+    beamObject = document.addObject("Part::FeaturePython", "TestBeam")
+    width = 100
+    height = 100
+    Beam(beamObject, line, width, height)
     document.recompute()
     BeamGui.ViewProviderBox(beamObject.ViewObject)
-
-    return beamObject
-
--- Mortise.py
+++ Mortise.py
@@ -10,21 +10,21 @@ class Mortise():
         obj.Proxy = self
 
         obj.addProperty('App::PropertyString', 'Description', 'Base', 'Joint description').Description = "Mortise and tenon joint"
-        obj.addProperty('App::PropertyEnumeration', 'Type', 'Base', 'Joint type').Type = ["hole","tongue"]
-        obj.Type = "hole"
+        obj.addProperty('App::PropertyEnumeration', 'Type', 'Base', 'Joint type').Type = ["mortise","tenon"]
+        obj.Type = "mortise"
 
-        obj.addProperty('App::PropertyLength', 'HoleLength', 'Dimensions', 'Mortise length').HoleLength = '60 mm'
-        obj.addProperty('App::PropertyLength', 'HoleWidth', 'Dimensions', 'Mortise width').HoleWidth = '30 mm'
-        obj.addProperty('App::PropertyLength', 'StockLength', 'Dimensions', 'Stock length').StockLength = '102 mm'
         obj.addProperty('App::PropertyLength', 'StockWidth', 'Dimensions', 'Stock width').StockWidth = '102 mm'
-        obj.addProperty('App::PropertyLength', 'Depth', 'Dimensions', 'Mortise depth').Depth = '60 mm'
+        obj.addProperty('App::PropertyLength', 'StockHeight', 'Dimensions', 'Stock length').StockHeight = '102 mm'
+        obj.addProperty('App::PropertyLength', 'MortiseWidth', 'Dimensions', 'Mortise width').MortiseWidth = '30 mm'
+        obj.addProperty('App::PropertyLength', 'MortiseLength', 'Dimensions', 'Mortise length').MortiseLength = '60 mm'
+        obj.addProperty('App::PropertyLength', 'MortiseDepth', 'Dimensions', 'Mortise depth').MortiseDepth = '60 mm'
+
+        obj.addProperty('Part::PropertyPartShape', 'StockFace', 'Faces', 'Face defining stock')
+        obj.addProperty('Part::PropertyPartShape', 'MortiseFace', 'Faces', 'Face defining feature')
 
         obj.addProperty('App::PropertyVector', 'TemporaryPosition', 'Orientation', 'Temporary mortise position').TemporaryPosition = FreeCAD.Vector(0,0,0)
         obj.addProperty('App::PropertyVector', 'TemporaryNormal', 'Orientation', 'Temporary mortise normal').TemporaryNormal = FreeCAD.Vector(0,0,1)
         obj.addProperty('App::PropertyVector', 'TemporaryDirection', 'Orientation', 'Temporary mortise direction').TemporaryDirection = FreeCAD.Vector(0,1,0)
-
-        obj.addProperty('Part::PropertyPartShape', 'HoleFace', 'Faces', 'Face defining feature')
-        obj.addProperty('Part::PropertyPartShape', 'StockFace', 'Faces', 'Face defining stock')
 
         obj.addProperty('App::PropertyVector', 'Position', 'Orientation', 'Mortise position').Position = FreeCAD.Vector(0,0,0)
         obj.addProperty('App::PropertyVector', 'Normal', 'Orientation', 'Mortise normal').Normal = FreeCAD.Vector(0,0,1)
@@ -34,11 +34,11 @@ class Mortise():
 
         self.execute(obj)
 
-    def getHoleShapes(self, obj):
+    def getMortiseFace(self, obj):
         "Return temporary shape created at the origin and in a default orientation"
 
-        length = obj.HoleLength.Value
-        width = obj.HoleWidth.Value
+        length = obj.MortiseLength.Value
+        width = obj.MortiseWidth.Value
 
         ## Points in each quadrant
         point0 = FreeCAD.Vector(+width/2, +length/2-width/2, 0)
@@ -58,23 +58,21 @@ class Mortise():
         arc32 = Part.Edge(Part.Arc(point3,point32,point2))
         
         ## Face and Shape
-        holeWire = Part.Wire([line03,arc32,line21,arc10])
-        holeFace = Part.Face(holeWire)
-        holeShape = holeFace.extrude(-obj.Depth.Value * obj.TemporaryNormal)
+        mortiseWire = Part.Wire([line03,arc32,line21,arc10])
+        mortiseFace = Part.Face(mortiseWire)
 
-        return holeFace, holeShape
+        return mortiseFace
         
-    def getStockShapes(self, obj):
+    def getStockFace(self, obj):
         "Return temporary shape created at the origin and in a default orientation"
 
-        length = obj.StockLength.Value
+        height = obj.StockHeight.Value
         width = obj.StockWidth.Value
 
-        centerPoint = FreeCAD.Vector(-length/2, -width/2, 0)
-        stockFace = Part.makePlane(length, width, centerPoint)
-        stockShape = stockFace.extrude(-obj.Depth.Value * obj.TemporaryNormal)
+        centerPoint = FreeCAD.Vector(-height/2, -width/2, 0)
+        stockFace = Part.makePlane(height, width, centerPoint)
 
-        return stockFace, stockShape
+        return stockFace
 
     def addOperation(self, obj):
         "Create an adaptive machining operation using the Path workbench"
@@ -82,21 +80,20 @@ class Mortise():
         document = obj.Document
         objectAdaptive = document.addObject("Path::FeaturePython", "Adaptive")
         PathAdaptive.PathAdaptive(objectAdaptive)
-        objectAdaptive.Base = (obj, ['HoleFace'])    # App::PropertyLinkSub
+        objectAdaptive.Base = (obj, ['MortiseFace'])    # App::PropertyLinkSub
         objectAdaptive.Stock = (obj, ['StockFace'])  # App::PropertyLinkSub
 
     def execute(self, obj):
         "Executed on document recomputes"
 
-        holeFace, holeShape = self.getHoleShapes(obj)
-        stockFace, stockShape = self.getStockShapes(obj)
-        # store faces for access by other objects
-        obj.HoleFace = holeFace
-        obj.StockFace = stockFace
-        cutoutShape = stockShape.cut(holeShape)
+        obj.MortiseFace = self.getMortiseFace(obj)
+        obj.StockFace = self.getStockFace(obj)
+        stockShape = obj.StockFace.extrude(-obj.MortiseDepth.Value * obj.TemporaryNormal)
+        mortiseShape = obj.MortiseFace.extrude(-obj.MortiseDepth.Value * obj.TemporaryNormal) 
+        cutoutShape = stockShape.cut(mortiseShape)
 
-        if obj.Type == "hole":
-            obj.Shape = holeShape
+        if obj.Type == "mortise":
+            obj.Shape = mortiseShape
         else:
             obj.Shape = cutoutShape
 
@@ -112,33 +109,35 @@ class Mortise():
             obj.OperationExists = True
 
 def test():
-    """ Add some mortises to a document, for testing """
+    ''' 
+    Create a few Mortise objects and add them to a document, for testing
+    ''' 
 
     document = FreeCAD.ActiveDocument
     if not document: 
         document = FreeCAD.newDocument()
-
+    
     # create some features for testing
-    features = [] # orientation = (position, normal, direction, type)
-
+    features = []
+    
     position = FreeCAD.Vector(0,50,50)
     normal = FreeCAD.Vector(-1,0,0)
     direction = FreeCAD.Vector(0,1,0)
-    type = "tongue"
+    type = "tenon"
     features.append((position, normal, direction, type))
-
+    
     position = FreeCAD.Vector(200,0,50)
     normal = FreeCAD.Vector(0,-1,0)
     direction = FreeCAD.Vector(1,0,0)
-    type = "hole"
+    type = "mortise"
     features.append((position, normal, direction, type))
-
+    
     position = FreeCAD.Vector(1000,50,50)
     normal = FreeCAD.Vector(1,0,0)
     direction = FreeCAD.Vector(0,1,0)
-    type = "tongue"
+    type = "tenon"
     features.append((position, normal, direction, type))
-
+    
     objects = []
     for (position, normal, direction, type) in features:
         objectMortise = document.addObject("Part::FeaturePython", "Mortise")
@@ -149,11 +148,10 @@ def test():
         objectMortise.Direction = direction
         objectMortise.Type = type
         objects.append(objectMortise)
-
+    
     # Fusion of features
     objectFusion = document.addObject("Part::MultiFuse","Features")
     objectFusion.Shapes = objects
-
+    
     document.recompute()
-    return objects
 
